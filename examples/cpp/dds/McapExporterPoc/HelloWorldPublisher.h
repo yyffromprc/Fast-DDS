b041e113714bd2b7ffb0530e8ce85a2f06380c2b
RaulSanchez <raul@eprosima.com>
2023-01-03 15:58:32 +0100

    PoC: exporting Fast DDS data to MCAP
    Signed-off-by: RaulSanchez <raul@eprosima.com>

---
+++ examples/cpp/dds/McapExporterPoc/HelloWorldPublisher.h
@@ -0,0 +1,75 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file HelloWorldPublisher.h
+ *
+ */
+
+#ifndef HELLOWORLDPUBLISHER_H_
+#define HELLOWORLDPUBLISHER_H_
+
+#include "HelloWorldPubSubTypes.h"
+
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+
+#include <mcap/writer.hpp>
+
+class HelloWorldPublisher
+{
+public:
+
+    HelloWorldPublisher();
+
+    virtual ~HelloWorldPublisher();
+
+    //!Initialize
+    bool init();
+
+    //!Publish a sample
+    bool publish();
+
+    //!Run publisher
+    void run(
+            uint32_t sleep);
+
+    // Get current timestamp
+    mcap::Timestamp now();
+
+private:
+
+    HelloWorld hello_;
+    eprosima::fastdds::dds::DomainParticipant* participant_;
+    eprosima::fastdds::dds::Publisher* publisher_;
+    eprosima::fastdds::dds::Topic* topic_;
+    eprosima::fastdds::dds::DataWriter* writer_;
+    eprosima::fastdds::dds::TypeSupport type_;
+
+    bool stop_;
+    void run_thread(
+            uint32_t sleep);
+
+
+    mcap::McapWriter mcap_writer_;
+    mcap::Schema helloworld_schema;
+    mcap::Channel helloworld_channel;
+    static const char* SCHEMA_NAME;
+    static const char* SCHEMA_TEXT;
+};
+
+
+
+#endif /* HELLOWORLDPUBLISHER_H_ */
---
+++ examples/cpp/dds/McapExporterPoc/README.md
@@ -0,0 +1,30 @@
+# Dependencies
+
+* MCAP: header only library already available in PoC folder
+* [lz4](https://lz4.github.io/lz4/) (requiered)
+* [zstd](https://facebook.github.io/zstd/) (requiered)
+
+# Build instructions
+
+```bash
+source ~/fastdds/install/setup.zsh
+cd ~/fastdds/src/fastrtps/examples/cpp/dds/McapExporterPoc
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j8
+touch output.mcap
+```
+
+# Run PoC
+
+* Terminal
+
+    ```bash
+    ./McapExporterPoc publisher
+    ```
+
+* Foxglove Studio
+
+    * Open Foxglove Studio (desktop or web application)
+    * *Open local file -> output.mcap*
+    * Load ``resources/fastdds_poc_layout.json`` file as existing layout or click *Add panel -> Data Source Info / Table / Plot / Raw Messages* to create the layout from scratch.
+
