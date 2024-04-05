5d5ab500d4b1b7a7873c46b85cb397110bb374ed
cferreiragonz <carlosferreira@eprosima.com>
2024-04-05 09:41:22 +0200

    Refs #20629: Refactor PDPClient
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- include/fastdds/rtps/attributes/ServerAttributes.h
+++ include/fastdds/rtps/attributes/ServerAttributes.h
@@ -61,7 +61,6 @@ public:
         guidPrefix = fastrtps::rtps::GuidPrefix_t::unknown();
         metatrafficUnicastLocatorList.clear();
         metatrafficMulticastLocatorList.clear();
-        is_connected = false;
     }
 
     FASTDDS_EXPORTED_API fastrtps::rtps::GUID_t GetParticipant() const;
@@ -82,9 +81,6 @@ public:
 
     //!Guid prefix
     fastrtps::rtps::GuidPrefix_t guidPrefix;
-
-    // Whether connection has been established
-    bool is_connected = false;
 
     // Check if there are specific transport locators associated
     // the template parameter is the locator kind (e.g. LOCATOR_KIND_UDPv4)
