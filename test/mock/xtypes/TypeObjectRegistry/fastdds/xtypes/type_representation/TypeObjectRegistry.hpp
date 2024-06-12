e35fb1c823d6925a7bf7a0fb869da33468c3c26d
Ricardo González Moreno <ricardo@richiware.dev>
2024-06-12 17:35:54 +0200

    Refs #20165. Fix for future rebase
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/mock/xtypes/TypeObjectRegistry/fastdds/xtypes/type_representation/TypeObjectRegistry.hpp
+++ test/mock/xtypes/TypeObjectRegistry/fastdds/xtypes/type_representation/TypeObjectRegistry.hpp
@@ -86,6 +86,11 @@ public:
                 const TypeObject& type_object,
                 TypeIdentifierPair & /*type_identifier*/), (override));
 
+    MOCK_METHOD(ReturnCode_t, register_type_object, (
+                const TypeObject& type_object,
+                TypeIdentifierPair& /*type_identifier*/,
+                bool /*build_minimal*/));
+
     MOCK_METHOD(ReturnCode_t, register_typeobject_w_dynamic_type, (
                 const DynamicType::_ref_type& /*dynamic_type*/,
                 TypeIdentifierPair & /*type_ids*/), (override));
